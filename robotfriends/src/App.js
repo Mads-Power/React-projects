ee1aad49c900078a05d460d07c9c3f2bfb9d8bc5
Mads-Power <mr.kolkin@gmail.com>
2022-09-05 16:13:11 +0200

    scroll component

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -3,6 +3,7 @@ import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
@@ -44,7 +45,9 @@ class App extends Component {
         <div className="tc">
           <h1>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       );
     }
